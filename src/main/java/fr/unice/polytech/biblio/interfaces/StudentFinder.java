ac90ea597f2c5cb4e4d89fabd3021356f34b2381
Mireille Blay-Fornarino <mireilleBF@gmail.com>
2025-09-08 15:51:07 +0200

    remove interfaces no more used

--- src/main/java/fr/unice/polytech/biblio/interfaces/StudentFinder.java
+++
@@ -1,9 +0,0 @@
-package fr.unice.polytech.biblio.interfaces;
-
-import java.util.List;
-import java.util.Optional;
-public interface StudentFinder<T extends StudentInterface> {
-    List<T > findAll();
-    Optional<T> findByName(String name);
-    Optional<T> findByNumber(int studentNumber);
-}
