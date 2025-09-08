2c28087ef7fd3e29b78b5e6b49327ea26c4c45d1
Philippe Collet <philcollet@gmail.com>
2025-09-08 14:21:05 +0200

    refactor: use new configuration for cucumberTest class

--- src/test/java/fr/unice/polytech/biblio/RunCucumberTest.java
+++ src/test/java/fr/unice/polytech/biblio/RunCucumberTest.java
@@ -1,9 +1,6 @@
 package fr.unice.polytech.biblio;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
@@ -11,14 +8,10 @@ import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 
-//Specifies the location of the feature files. The feature files are stored in the "features/biblio" directory within the classpath.
-@SelectClasspathResource("features/biblio")
-
+@SelectPackages("features.biblio")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-//Specifies the package where the step definitions are located.
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "fr.unice.polytech.biblio")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "fr.unice.polytech.biblio.stepDefs")
 
 public class RunCucumberTest {
-    // will run all features found on the classpath
-    // in the same package as this class (can be changed with @SelectPackages)
+    // will run all features found on the configuration
 }
